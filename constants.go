7824452614328b9045bb5f940249a28521692406
amanbora856 <amanbora30172@gmail.com>
2025-01-18 20:20:53 +0530

    Learn the constants loginc

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const age = 30
+
+func main() {
+	name := 10
+	fmt.Println(age)
+	fmt.Println(name)
+	fmt.Println(2 + 2)
+	fmt.Println("code")
+	fmt.Println(5 / 9)
+}
