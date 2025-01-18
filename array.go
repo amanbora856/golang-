cb25a069bc3ef9350873aad1de0e04a290f7639e
amanbora856 <amanbora30172@gmail.com>
2025-01-19 00:51:18 +0530

    Done some learning on the array type

--- array.go
+++ array.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
 
 	var nums [5]int
+	var vals [8]bool
+	fmt.Println(vals)
 	fmt.Println(len(nums))
+	fmt.Println(nums) //default behaviour of array that 0 will be added;
 }
