4faa2fcf6df21ae994a65bbc59d00a6b5da1784e
amanbora856 <amanbora30172@gmail.com>
2025-01-19 00:55:53 +0530

    Learnt about the 2d array

--- array.go
+++ array.go
@@ -6,6 +6,10 @@ func main() {
 
 	var nums [5]int
 	var vals [8]bool
+	numss := [5]int{1, 2, 7, 8, 5}
+	num2Daray := [2][2]int{{1, 2}, {1, 2}}
+	fmt.Println(num2Daray)
+	fmt.Println(numss)
 	fmt.Println(vals)
 	fmt.Println(len(nums))
 	fmt.Println(nums) //default behaviour of array that 0 will be added;
